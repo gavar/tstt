2a1d184f69c85d2ba02102a3f7f3ba014986c00c
Max Stankevich <max.stankevich.me@gmail.com>
2019-10-10 02:34:17 +0300

    feat: new primitive type - Falsy

--- src/primitive.ts
+++ src/primitive.ts
@@ -4,6 +4,12 @@ export type Nil = null | undefined;
 /** Make type `T` assignable to {@link Nil}. */
 export type Nilable<T> = T extends any ? T | Nil : never;
 
+/**
+ * Value that is considered false when encountered in a Boolean context.
+ * https://developer.mozilla.org/en-US/docs/Glossary/falsy
+ */
+export type Falsy = false | 0 | 0n | "" | null | undefined;
+
 /** All the possible values returned by {@code typeof} operator. */
 export type TypeOf =
   | "undefined"
