18b55688561a21c08d337a5ee77eefdcb2b66ae4
Max Stankevich <max.stankevich.me@gmail.com>
2019-10-11 20:24:14 +0300

    feat: truthy type

--- src/primitive.ts
+++ src/primitive.ts
@@ -5,10 +5,16 @@ export type Nil = null | undefined;
 export type Nilable<T> = T extends any ? T | Nil : never;
 
 /**
- * Value that is considered false when encountered in a Boolean context.
+ * Value that is considered false when encountered in a {@link Boolean} context.
  * https://developer.mozilla.org/en-US/docs/Glossary/falsy
  */
-export type Falsy = false | 0 | 0n | "" | null | undefined;
+export type Falsy = false | 0 | "" | null | undefined;
+
+/**
+ * Value that is considered true when encountered in a {@link Boolean} context.
+ * https://developer.mozilla.org/en-US/docs/Glossary/truthy
+ */
+export type Truthy<T = any> = T extends Falsy ? never : T;
 
 /** All the possible values returned by {@code typeof} operator. */
 export type TypeOf =
